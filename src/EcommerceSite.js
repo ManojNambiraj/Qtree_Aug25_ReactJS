9657aff9693f7d3ec590a8ed148cfd7a2ad8bed8
ManojNambiraj <postboxmano@gmail.com>
2025-09-08 19:41:52 +0530

    ecommerce datas

--- src/EcommerceSite.js
+++ src/EcommerceSite.js
@@ -1,19 +1,46 @@
 import React from "react";
 import "./EcommerceSite.css";
 import ProductCard from "./compenents/ProductCard";
+import img1 from "./assets/images/redmi.jpg"
+import img2 from "./assets/images/img2.jpg"
+import img3 from "./assets/images/img2.webp"
 
 function EcommerceSite() {
+
+  const datas = [
+    {
+      id: 1,
+      name: "Apple",
+      price: 120000,
+      desc: "Some quick example text to build on the card title",
+      img: img1,
+    },
+    {
+      id: 2,
+      name: "Redmi",
+      price: 15000,
+      desc: "Some quick example text to build on the card title",
+      img: img2,
+    },
+    {
+      id: 3,
+      name: "Sony",
+      price: 47000,
+      desc: "Some quick example text to build on the card title",
+      img: img3,
+    },
+  ];
+
   return (
     <div className="container-fluid demomain">
       <div className="row">
         <div className="left col-10">
-          <div className="row m-4 d-flex g-2">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+          <div className="row m-2 d-flex g-2">
+            {
+              datas.map((product) => {
+                return <ProductCard product={product} />;
+              })
+            }
           </div>
         </div>
         <div className="right col-2">right</div>
--- src/compenents/ProductCard.js
+++ src/compenents/ProductCard.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import img1 from "../assets/images/redmi.jpg"
+import React from "react";
+import img1 from "../assets/images/redmi.jpg";
 
-function ProductCard() {
+function ProductCard(props) {
   return (
-    <div className="card mr-2" style={{ width: "18rem" }}>
-      <img src={img1} className="card-img-top" alt="..." />
+    <div className="card m-2" style={{ width: "18rem" }}>
+      <img src={props.product.img} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
+        <h5 className="card-title">{props.product.name}</h5>
+        <p className="card-text">{props.product.price}</p>
+        <p className="card-text">{props.product.desc}</p>
         <button className="btn btn-primary">Add to cart</button>
       </div>
     </div>
   );
 }
 
-export default ProductCard
+export default ProductCard;
