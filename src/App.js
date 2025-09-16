b0b0b2c50485d288b8acfe5d394dc671bdf37962
ManojNambiraj <postboxmano@gmail.com>
2025-09-16 20:33:08 +0530

    CRUD App Get method

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@
 // import Intro from './Intro';
 // import LikeCounter from './LikeCounter';
 // import Routing from './Routing';
-import EcommerceSite from "./EcommerceSite";
+// import EcommerceSite from "./EcommerceSite";
+import UserManagement from "./UserManagement";
 
 function App() {
   return (
     <div className="App">
-      <EcommerceSite />
+
+      <UserManagement />
+
+      {/* <EcommerceSite /> */}
 
       {/* <h1>Hello world</h1> */}
 
       {/* <Routing /> */}
 
       {/* <LikeCounter /> */}
-      
+
       {/* <Intro /> */}
     </div>
   );
---
+++ src/UserManagement.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function UserManagement() {
+
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+      getData()
+    }, []);
+
+    const getData = async () => {
+        const studentsDatas = await axios.get(`https://68c97466ceef5a150f64f507.mockapi.io/students`);
+
+        setUserData(studentsDatas.data);
+    }
+  
+
+
+  return (
+    <div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Email</th>
+            <th scope="col">Mobile</th>
+            <th scope="col">Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((item, index) => {
+            return (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.email}</td>
+                <td>{item.mobile}</td>
+                <td>{item.password}</td>
+              </tr>
+            );
+          })}
+          
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UserManagement;
+
+
+// HTTP methods  --> POST      GET       PUT     DELETE
+//   CRUD            Create    Read      Update  Delete
